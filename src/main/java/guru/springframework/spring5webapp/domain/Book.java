e853a0452ec8fb89a75e2401982fb42a4c78eb27
Vinícius <viniciusfloresribeiro@gmail.com>
2022-07-22 14:33:54 -0300

    author controller

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -72,7 +72,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
